5aa2e217d0a281685bed9eb6c197169d3df7fe0b
Anythingsoup01 <143284638+Anythingsoup01@users.noreply.github.com>
2024-06-26 09:03:22 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -42,49 +42,4 @@ project "ImGui"
 		runtime "Release"
 		optimize "on"
         symbols "off"
-=======
-project "ImGui"
-	kind "StaticLib"
-	language "C++"
-    staticruntime "On"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
->>>>>>> 96a66535ec7c8b3d5d042a1ec05975167009962c
