43f97c5df5c553e3407bf1590492610be86859cc
Anythingsoup01 <williamenglish1220@gmail.com>
2025-04-27 15:35:37 -0400

    Updated Main Branch with Prebuild

---
+++ prebuild.lua
@@ -0,0 +1,21 @@
+workspace "ImGui-Prebuild"
+
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    dialect "17"
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
